fae6c6d7c27018981f35c8ac89715310aee1d7a5
baka943 <gzmn@vip.qq.com>
2022-04-22 23:47:34 +0800

    先加个标记Text.of

--- kubejs/startup_scripts/caidan.js
+++ kubejs/startup_scripts/caidan.js
@@ -1,6 +1,6 @@
 
 onEvent('item.registry', event => {
     event.create('nightbreak', item => {
-		item.displayName('§6§l作者的头像').tooltip('§7你是怎么发现我的？')
+		item.displayName('§6§l作者的头像').tooltip(Text.of('§7你是怎么发现我的？'))
 	  })
 })
--- kubejs/startup_scripts/eden_ring_ingot.js
+++ kubejs/startup_scripts/eden_ring_ingot.js
@@ -1,23 +1,23 @@
 onEvent('item.registry', event => {
     event.create('balloon_ingot', item => {
-		item.displayName('气球蘑菇锭').tooltip('§7在伊甸之环找到原材料')
+		item.displayName('气球蘑菇锭').tooltip(Text.of('§7在伊甸之环找到原材料'))
 	  })
 })
 
 onEvent('item.registry', event => {
     event.create('auritis_ingot', item => {
-		item.displayName('曙光锭').tooltip('§7在伊甸之环找到原材料')
+		item.displayName('曙光锭').tooltip(Text.of('§7在伊甸之环找到原材料'))
 	  })
 })
 
 onEvent('item.registry', event => {
     event.create('gravilite_ingot', item => {
-		item.displayName('重晶石锭').tooltip('§7在伊甸之环找到原材料')
+		item.displayName('重晶石锭').tooltip(Text.of('§7在伊甸之环找到原材料'))
 	  })
 })
 
 onEvent('item.registry', event => {
     event.create('eden_ingot', item => {
-		item.displayName('伊甸锭').tooltip('§7在伊甸之环找到原材料')
+		item.displayName('伊甸锭').tooltip(Text.of('§7在伊甸之环找到原材料'))
 	  })
 })
