d7b724150471ef1837cf3e33d4342075c27cef8d
baka943 <gzmn@vip.qq.com>
2022-04-22 23:33:15 +0800

    Infinity Book Name Translate

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -1,7 +1,7 @@
 onEvent('item.tooltip', tooltip => {
     
-    tooltip.add(['patchouli:guide_book'], '《无限》整合包的指南书')
-    tooltip.add(['edenring:guide_book'], '这本书暂时不要打开')
-    tooltip.add(['betterend:guidebook'], '这本书暂时无法打开')
-    tooltip.add(['victus:victus_journal'], '已被魔改，该书作废')
+    tooltip.add('patchouli:guide_book', Text.translate('text.infinity.book.tooltip'))
+    tooltip.add('edenring:guide_book', '这本书暂时不要打开')
+    tooltip.add('betterend:guidebook', '这本书暂时无法打开')
+    tooltip.add('victus:victus_journal', '已被魔改，该书作废')
   })
